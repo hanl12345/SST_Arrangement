6588e6a82712338b35ba5d6ef96d435facaf363a
hanl12345 <lidehanc827@gmail.com>
2026-01-17 17:57:04 -0600

    Add files via upload

--- makeRegeneie_GWASWorkflow_Phecode_SubmitTable_allXall.sh
+++ makeRegeneie_GWASWorkflow_Phecode_SubmitTable_allXall.sh
@@ -12,20 +12,15 @@
 
 out_tsv="Regenie_GWAS35K_Phecode_Table.tsv"
 
-echo -e "idx\tSuperPop\tphenoColList_File\toutBucket_prefix\tkeepFile" > "${out_tsv}"
+echo -e "idx\tSuperPop\tphenoColList_File\toutBucket_prefix" > "${out_tsv}"
 
 idx=1
 
 for pop in AFR EUR; do
-  for i in $(seq -w 0 17); do
+  for i in $(seq -w 0 18); do
     pheno_file="gs://fc-secure-540f27be-97ea-4ffd-adb7-c195458eb278/Regenie_GWAS35KResultV2/PhecodeList_AGD35K_Sub/IndPhecodeList_AGD35K_Part_${i}.txt"
-
-    out_bucket="gs://fc-secure-540f27be-97ea-4ffd-adb7-c195458eb278/Regenie_GWAS35KResultV2/Phecode_${pop}Prob05/Phecode"
-
-    keep_file="gs://fc-secure-540f27be-97ea-4ffd-adb7-c195458eb278/Regenie_allxall/AllxAll_AGD35KSampleClass/AGD35KQV2_SupervisedProb0.5_${pop}_Subset.txt"
-
-    echo -e "${idx}\t${pop}\t${pheno_file}\t${out_bucket}\t${keep_file}" >> "${out_tsv}"
-
+    out_bucket="gs://fc-secure-540f27be-97ea-4ffd-adb7-c195458eb278/Regenie_GWAS35KResultV2/RegenieWorkflowV1/Phecode_${pop}Prob05/Phecode_${pop}"
+    echo -e "${idx}\t${pop}\t${pheno_file}\t${out_bucket}" >> "${out_tsv}"
     idx=$((idx + 1))
   done
 done
